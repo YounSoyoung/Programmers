6df4f31cd78f9f565443a0775a228a6402f8804f
YounSoyoung <grace902853@gmail.com>
2023-06-13 14:45:20 +0900

    hash 연습

---
+++ hash/Pokemon.java
@@ -0,0 +1,20 @@
+package hash;
+
+import java.util.*;
+
+class Pokemon {
+    public int solution(int[] nums) {
+        int max = nums.length/2;
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int num : nums){
+            set.add(num);
+        }
+
+        if(set.size() > max){
+            return max;
+        }else return set.size();
+
+    }
+}
