8486701b4a4ff9b6eeea30ac8b5a4a65ac46d7ab
YounSoyoung <grace902853@gmail.com>
2023-06-21 23:25:10 +0900

    정렬

---
+++ sort/Hindex.java
@@ -0,0 +1,24 @@
+package sort;
+
+
+import java.util.*;
+
+class Hindex {
+    public int solution(int[] citations) {
+        int answer = 0;
+        //0 1 3 5 6
+
+        Arrays.sort(citations);
+
+        for (int i = 0; i < citations.length; i++) {
+            int h = citations.length - i;
+
+            if (citations[i] >= h) {
+                answer = h;
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
